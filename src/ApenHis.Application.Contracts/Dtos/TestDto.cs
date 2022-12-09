c908aa762fdf04f7b0b261ac1ac42628d44f0c2d
aihui109 <aihui109@msn.com>
2022-12-09 14:01:34 +0800

    测试elsa代码

---
+++ src/ApenHis.Application.Contracts/Dtos/TestDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApenHis.Dtos
+{
+    public class TestDto
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
---
+++ src/ApenHis.Application.Contracts/Dtos/TestInput.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApenHis.Dtos
+{
+    public class TestInput
+    {
+        public string UserName1 { get; set; }
+        public string Password1 { get; set; }
+        public string PhoneNumber1 { get; set; }
+    }
+}
