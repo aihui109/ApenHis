c90676d1a738f1ac821c95f8438da0b9d23e2518
aihui109 <aihui109@msn.com>
2022-12-16 15:27:58 +0800

    20221216063958_扩展属性和RoleFeature

--- src/ApenHis.EntityFrameworkCore/Migrations/20221216063958_扩展属性和RoleFeature.Designer.cs
+++ src/ApenHis.EntityFrameworkCore/Migrations/20221216063958_扩展属性和RoleFeature.Designer.cs
@@ -4,6 +4,7 @@ using ApenHis.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Volo.Abp.EntityFrameworkCore;
 
@@ -12,9 +13,10 @@ using Volo.Abp.EntityFrameworkCore;
 namespace ApenHis.Migrations
 {
     [DbContext(typeof(ApenHisDbContext))]
-    partial class ApenHisDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221216063958_扩展属性和RoleFeature")]
+    partial class 扩展属性和RoleFeature
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/ApenHis.EntityFrameworkCore/Migrations/20221216063958_扩展属性和RoleFeature.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ApenHis.Migrations
+{
+    public partial class 扩展属性和RoleFeature : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "InputCode",
+                table: "AbpUsers",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true,
+                comment: "输入码");
+
+            migrationBuilder.CreateTable(
+                name: "Features",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ParentGuid = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    Title = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    IsMenu = table.Column<bool>(type: "bit", nullable: false),
+                    PicUrl = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DOM = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UpdateTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    SortNo = table.Column<int>(type: "int", nullable: true),
+                    IsRoot = table.Column<bool>(type: "bit", nullable: false),
+                    IsHidden = table.Column<bool>(type: "bit", nullable: false),
+                    Url = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IsLog = table.Column<bool>(type: "bit", nullable: false),
+                    CreationTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false, defaultValue: false),
+                    Memo = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    TenantId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    IsMobile = table.Column<bool>(type: "bit", nullable: false),
+                    ConcurrencyStamp = table.Column<string>(type: "nvarchar(40)", maxLength: 40, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Features", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RoleFeatures",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    RoleGuid = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    FeatureGuid = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CreationTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatorId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RoleFeatures", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "UIDXFeatureTenantIdTitle",
+                table: "Features",
+                columns: new[] { "TenantId", "Title" },
+                unique: true,
+                filter: "[TenantId] IS NOT NULL AND [Title] IS NOT NULL");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Features");
+
+            migrationBuilder.DropTable(
+                name: "RoleFeatures");
+
+            migrationBuilder.DropColumn(
+                name: "InputCode",
+                table: "AbpUsers");
+        }
+    }
+}
