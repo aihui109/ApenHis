8187dd2e6a95c0f0dde63b86f44fdf725a30ee0d
aihui109 <aihui109@msn.com>
2023-04-12 10:04:23 +0800

    Odata加入授权

--- src/ApenHis.HttpApi/Controllers/ODataTestController.cs
+++ src/ApenHis.HttpApi/Controllers/ODataTestController.cs
@@ -6,11 +6,11 @@ using Volo.Abp.Domain.Repositories;
 using Volo.Abp.Identity;
 using Microsoft.AspNetCore.OData.Routing.Attributes;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
 
 namespace ApenHis.Controllers
 {
-    [ODataAttributeRouting]
-    [Route("odata")]
+    [Authorize, ODataAttributeRouting, Route("odata")]
     public class ODataTestController : ApenHisController
     {
         private readonly IRepository<IdentityUser, Guid> _userRepository;
@@ -20,9 +20,7 @@ namespace ApenHis.Controllers
             _userRepository = userRepository;
         }
 
-        // GET
-        [EnableQuery]
-        [HttpGet("users")]
+        [EnableQuery,HttpGet("users")]
         public virtual async Task<IQueryable<IdentityUser>> GetAsync()
         {
             return await _userRepository.GetQueryableAsync();
