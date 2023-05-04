8e1a1805e8044fc78fde848180ed0fcae86febba
aihui109 <aihui109@msn.com>
2023-05-04 15:22:18 +0800

    abp升级到7.2

--- src/ApenHis.EntityFrameworkCore/EntityFrameworkCore/ApenHisDbContext.cs
+++ src/ApenHis.EntityFrameworkCore/EntityFrameworkCore/ApenHisDbContext.cs
@@ -47,7 +47,7 @@ public class ApenHisDbContext :
     public DbSet<OrganizationUnit> OrganizationUnits { get; set; }
     public DbSet<IdentitySecurityLog> SecurityLogs { get; set; }
     public DbSet<IdentityLinkUser> LinkUsers { get; set; }
-
+    public DbSet<IdentityUserDelegation> UserDelegations { get; set; }
     // Tenant Management
     public DbSet<Tenant> Tenants { get; set; }
     public DbSet<TenantConnectionString> TenantConnectionStrings { get; set; }
--- src/ApenHis.EntityFrameworkCore/Migrations/20230504065655_Upgraded_To_Abp_7_2.Designer.cs
+++ src/ApenHis.EntityFrameworkCore/Migrations/20230504065655_Upgraded_To_Abp_7_2.Designer.cs
@@ -4,6 +4,7 @@ using ApenHis.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Volo.Abp.EntityFrameworkCore;
 
@@ -12,9 +13,11 @@ using Volo.Abp.EntityFrameworkCore;
 namespace ApenHis.Migrations
 {
     [DbContext(typeof(ApenHisDbContext))]
-    partial class ApenHisDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230504065655_Upgraded_To_Abp_7_2")]
+    partial class Upgraded_To_Abp_7_2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
